a8bf2f7ea56d92107484dbb53840dccb196620e0
janeezy <janeezyofficial@gmail.com>
2023-04-19 14:35:19 +0100

    updated html and css styling added javascrip

--- Starter/scripts/scripts.js
+++ Starter/scripts/scripts.js
@@ -3,3 +3,7 @@ document
 	.addEventListener('click', () =>
 		document.querySelector('.main-menu').classList.toggle('show')
 	);
+
+const date = document.querySelector('.copy');
+
+date.innerText = 'Copyright' + new Date().getFullYear();
